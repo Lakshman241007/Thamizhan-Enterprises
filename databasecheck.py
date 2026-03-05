6e1424bfa79638a044954b4b0fa1c14e07a41f77
SirThamizhan <kk1416307@gmail.com>
2026-03-05 20:30:55 +0530

    test test

--- databasecheck.py
+++ databasecheck.py
@@ -5,6 +5,10 @@ import os
 
 SUPABASE_URL = os.getenv("SUPA_URL")
 SUPABASE_KEY = os.getenv("SUPA_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Missing Supabase credentials")
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 data={
